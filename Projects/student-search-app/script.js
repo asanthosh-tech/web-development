0db486f75a2b8d7629888cfcfe764b5d51d19cf3
Santhosh Aavula <asanthosh08.tech@gmail.com>
2026-06-19 21:50:48 +0530

    Update script.js

--- Projects/student-search-app/script.js
+++ Projects/student-search-app/script.js
@@ -1,69 +1,86 @@
 //accessing elements
 const name = document.getElementById("name")
 const addbtn = document.getElementById("addbtn")
+const removebtn = document.getElementById("removebtn")
 const searchname = document.getElementById("searchname")
 const searchbtn = document.getElementById("searchbtn")
 const allbtn = document.getElementById("allbtn")
-const passcount = document.getElementById("passcount")
 const count = document.getElementById("count")
 const list = document.getElementById("list")
 const result = document.getElementById("result")
 
 //array for students
-let students = []
+let students = JSON.parse(localStorage.getItem("students")) || []
+
+result.style.display = "none"
+count.style.display = "none"
 
 // fucntions
+function saveStudents() {
+  localStorage.setItem("students", JSON.stringify(students));
+}
+function displayStudents(stdarr) {
+  list.innerHTML = ""
+  stdarr.forEach(i => {
+    let li = document.createElement("li")
+    let {
+      name, marks
+    } = i
+    li.textContent = `${name}-${marks}`
+    list.appendChild(li)
+  })
+}
 function add() {
-  //  console.log("hello")
-  if (name.value) {
-    let rand = 20+(100-20)*Math.random()
+  if (name.value.trim()) {
+    const rand = 20+(100-20)*Math.random()
     let std = {
       name: name.value,
       marks: Math.ceil(rand)
     }
-    students.push(std)
-    count.textContent = `Total Students:${students.length}`
+    students.push(std);
+    saveStudents();
     name.value = ""
   }
 }
+function all() {
+  count.style.display = "block"
+  displayStudents(students)
+  count.textContent = `Total Students:${students.length}`
+}
 function search() {
+  list.innerHTML=""
+  let students = JSON.parse(localStorage.getItem("students"))
   let found = students.find(i => i.name === searchname.value)
-  if (searchname.value) {
+  if (searchname.value.trim()) {
     if (found) {
+      result.style.display = "block"
       result.textContent = `Search Result: ${found.name}-${found.marks}`
+      searchname.value=""
     } else {
       result.textContent = `Not Found`
     }
   }
 }
-function all() {
-  list.innerHTML = ""
-  let allstds = students.map(i => {
-    return `<br>${i.name}-${i.marks}<br>`
-  }).join(" ");
-  list.innerHTML = `<b>All students:</b> ${students.length}<br> ${allstds}`
-}
 function pass() {
-  passcount.style.display = "block"
   let passed = students.filter(i => i.marks >= 35)
-  passcount.innerHTML = `Passed students:${passed.length}`
-
-  //showing passed stds
-  list.innerHTML = `<b>Passed Students:</b>${passed.length}`
-  passed.forEach(i => {
-    console.log("pass")
-    let li = document.createElement("li")
-    li.textContent = `${i.name}-${i.marks}`
-    list.appendChild(li)
-  })
+  count.innerHTML = `Passed students:${passed.length}`
+  displayStudents(passed)
 }
+function remove() {
+  if (students.length) {
+    students.pop();
+    saveStudents();
+    displayStudents(students)
+    count.textContent = `Total Students:${students.length}`
+  }
+}
+/*
+*/
 
 //button functionalities
-addbtn.addEventListener("click", add);
 searchbtn.addEventListener("click", search)
-allbtn.addEventListener("click", all)
 passbtn.addEventListener("click", pass)
-
-
-passcount.style.fontWeight = "bold"
-passcount.style.display = "none"
+allbtn.addEventListener("click", all)
+addbtn.addEventListener("click", add);
+name.addEventListener("keypress", add);
+removebtn.addEventListener("click", remove);
